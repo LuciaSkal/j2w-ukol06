046b6ae765a8d250751aed0a261228172ba659be
LuciaSkal <skalinovalucia@gmail.com>
2022-11-17 18:00:36 +0100

    pridani funkci

---
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
@@ -0,0 +1,64 @@
+package cz.czechitas.java2webapps.ukol6.controller;
+
+import cz.czechitas.java2webapps.ukol6.entity.Vizitka;
+import cz.czechitas.java2webapps.ukol6.VizitkaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+public class VizitkaController {
+    private final VizitkaRepository vizitkaRepository;
+
+    @Autowired
+    public VizitkaController(VizitkaRepository vizitkaRepository) {
+        this.vizitkaRepository = vizitkaRepository;
+    }
+
+    @InitBinder
+    public void nullStringBinding(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+    @GetMapping("/")
+    public ModelAndView getList() {
+        ModelAndView ModelAndView = new ModelAndView("seznam");
+        ModelAndView.addObject("seznam", vizitkaRepository.findAll());
+        return ModelAndView;
+    }
+
+    @GetMapping("/nova")
+    public Object addNew() {
+        return new ModelAndView("formular")
+                .addObject("vizitka", new Vizitka());
+    }
+
+    @PostMapping("/nova")
+    public Object pridat(@ModelAttribute("vizitka") @Valid Vizitka vizitka, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "formular";
+        }
+        vizitkaRepository.save(vizitka);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id:[0-9]+}")
+    public Object getDetail(@PathVariable long id) {
+        Optional<Vizitka> vizitka = vizitkaRepository.findById(id);
+        if (vizitka.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return new ModelAndView("vizitka")
+                .addObject("vizitka", vizitka.get());
+
+    }
+}
